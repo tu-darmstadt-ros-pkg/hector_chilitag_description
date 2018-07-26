08f44abd257fcd891633925d2e485c87203f7f81
Dorian Scholz <dorianscholz@gmx.de>
2018-07-26 14:24:07 +0200

    added chilitag mesh generator

---
+++ scripts/chilitag_mesh_generator.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname $(readlink -f $0))
+MESHES_DIR=$(readlink -f $BASE_DIR/../meshes)
+
+FIRST_ID=$1
+if [ -z $FIRST_ID ]; then
+    echo "usage $0 FIRST_ID [COUNT]"
+    exit 1
+fi
+
+COUNT=$2
+if [ -z $COUNT ]; then
+    COUNT=1
+fi
+LAST_ID=$((FIRST_ID+COUNT-1))
+
+for ((CHILITAG_ID=$FIRST_ID;CHILITAG_ID<=$LAST_ID;CHILITAG_ID++)); do
+    # create chilitag png
+    (cd ${MESHES_DIR}; PATH=$PATH:$ROS_WORKSPACE/../devel/bin chilitags-creator ${CHILITAG_ID} 15)
+    # insert chilitag id into template
+    CHILITAG_ID="$1" envsubst < "${MESHES_DIR}/chilitag_template.dae" > "${MESHES_DIR}/chilitag${CHILITAG_ID}.dae"
+done
