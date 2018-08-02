fe6f17b42666e7c406de365ebc127396f4e0c8fd
Dorian Scholz <dorianscholz@gmx.de>
2018-08-02 13:05:27 +0200

    remove use of ROS_WORKSPACE variable

--- scripts/chilitag_mesh_generator.sh
+++ scripts/chilitag_mesh_generator.sh
@@ -17,7 +17,7 @@ LAST_ID=$((FIRST_ID+COUNT-1))
 
 for ((CHILITAG_ID=$FIRST_ID;CHILITAG_ID<=$LAST_ID;CHILITAG_ID++)); do
     # create chilitag png
-    (cd ${MESHES_DIR}; PATH=$PATH:$ROS_WORKSPACE/../devel/bin chilitags-creator ${CHILITAG_ID} 15)
+    (cd ${MESHES_DIR}; chilitags-creator ${CHILITAG_ID} 15)
     # insert chilitag id into template
     CHILITAG_ID="${CHILITAG_ID}" envsubst < "${MESHES_DIR}/chilitag_template.dae" > "${MESHES_DIR}/chilitag${CHILITAG_ID}.dae"
 done
